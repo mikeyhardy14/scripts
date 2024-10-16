b1bcbd11630799b99816ccbd37403a3f4d9b3b83
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2024-10-16 13:07:44 -0400

    Update react_plotly_zoom.tsx

--- react_plotly_zoom.tsx
+++ react_plotly_zoom.tsx
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
-import Plot from 'react-plotly.js';
+import React, { useEffect, useState } from 'react';
 import Plotly from 'plotly.js-dist';
 
 const MultiPlotZoom: React.FC = () => {
   const chartIds = ['chart1', 'chart2', 'chart3']; // Add all your chart IDs here
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
+
+  const createCharts = () => {
+    const data1 = [{
+      x: ['2024-01-01', '2024-02-01', '2024-03-01', '2024-04-01'],
+      y: [10, 15, 13, 17],
+      type: 'scatter',
+      mode: 'lines+markers',
+      marker: { color: 'red' },
+    }];
+    const data2 = [{
+      x: ['2024-01-01', '2024-02-01', '2024-03-01', '2024-04-01'],
+      y: [16, 5, 11, 9],
+      type: 'scatter',
+      mode: 'lines+markers',
+      marker: { color: 'blue' },
+    }];
+    const data3 = [{
+      x: ['2024-01-01', '2024-02-01', '2024-03-01', '2024-04-01'],
+      y: [12, 9, 15, 12],
+      type: 'scatter',
+      mode: 'lines+markers',
+      marker: { color: 'green' },
+    }];
+
+    Plotly.newPlot('chart1', data1);
+    Plotly.newPlot('chart2', data2);
+    Plotly.newPlot('chart3', data3);
+  };
+
+  useEffect(() => {
+    createCharts();
+  }, []);
 
   const zoomCharts = () => {
     const start = new Date(startDate).getTime();
@@ -43,48 +74,9 @@ const MultiPlotZoom: React.FC = () => {
         />
         <button onClick={zoomCharts}>Zoom</button>
       </div>
-      <div id="chart1">
-        <Plot
-          data={[
-            {
-              x: ['2024-01-01', '2024-02-01', '2024-03-01', '2024-04-01'],
-              y: [10, 15, 13, 17],
-              type: 'scatter',
-              mode: 'lines+markers',
-              marker: { color: 'red' },
-            },
-          ]}
-          layout={{ width: 320, height: 240, title: 'Chart 1' }}
-        />
-      </div>
-      <div id="chart2">
-        <Plot
-          data={[
-            {
-              x: ['2024-01-01', '2024-02-01', '2024-03-01', '2024-04-01'],
-              y: [16, 5, 11, 9],
-              type: 'scatter',
-              mode: 'lines+markers',
-              marker: { color: 'blue' },
-            },
-          ]}
-          layout={{ width: 320, height: 240, title: 'Chart 2' }}
-        />
-      </div>
-      <div id="chart3">
-        <Plot
-          data={[
-            {
-              x: ['2024-01-01', '2024-02-01', '2024-03-01', '2024-04-01'],
-              y: [12, 9, 15, 12],
-              type: 'scatter',
-              mode: 'lines+markers',
-              marker: { color: 'green' },
-            },
-          ]}
-          layout={{ width: 320, height: 240, title: 'Chart 3' }}
-        />
-      </div>
+      <div id="chart1" style={{ width: '320px', height: '240px' }}></div>
+      <div id="chart2" style={{ width: '320px', height: '240px' }}></div>
+      <div id="chart3" style={{ width: '320px', height: '240px' }}></div>
     </div>
   );
 };
